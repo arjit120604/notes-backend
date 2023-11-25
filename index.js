a8dab094e0443d1e0a0bb7c4791c90fa1f0a51ea
arjit120604 <130384377+arjit120604@users.noreply.github.com>
2023-11-25 19:22:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // const bodyParser = require("body-parser");
 const app = express();
-const PORT = 5002;
+const PORT = process.env.PORT || 5002;
 const path = require("path");
 const cors = require("cors");
 
