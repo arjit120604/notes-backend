04ca7edfc089db65a9e0ea36af3bfdd47add0f80
arjit120604 <130384377+arjit120604@users.noreply.github.com>
2023-11-28 19:40:02 +0530

    added auth

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 5002;
 const path = require("path");
 const cors = require("cors");
+const userRouter = require("./routers/api/authRouter");
 
 app.use(cors());
 
@@ -11,6 +12,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/notes", require("./routers/api/notes"));
+app.use("/auth", userRouter);
+
+const dbUrl = process.env.MONGODB_URL;
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ routers/api/authRouter.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
+const router = express.Router();
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const UserModel = mongoose.model("users", userSchema);
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+  if (user) {
+    return res.status(400).json({ message: "Username already exists" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new UserModel({ username, password: hashedPassword });
+  await newUser.save();
+  res.json({ message: "User registered successfully" });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res
+      .status(400)
+      .json({ message: "Username or password is incorrect" });
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res
+      .status(400)
+      .json({ message: "Username or password is incorrect" });
+  }
+  const token = jwt.sign({ id: user._id }, "secret");
+  res.json({ token, userID: user._id });
+});
+
+module.exports = router;
+
+module.exports.verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    jwt.verify(authHeader, "secret", (err) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
